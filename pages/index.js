6eec1f508338ab5c8413911a81491cba8d5f698e
limakanorw <limakanorw@gmail.com>
2025-05-15 11:32:18 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>🎬 Filmy z Kamilem</h1>
+      <p>Witaj! To jest Twoja pierwsza własna strona 🌟</p>
+    </div>
+  );
+}
