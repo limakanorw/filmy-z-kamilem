4d72634108e3df220377f6fca3995d547b7a7cea
limakanorw <limakanorw@gmail.com>
2025-05-15 11:45:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,14 @@ export default function Home() {
     <div style={{ textAlign: "center", padding: "2rem" }}>
       <h1>🎬 Filmy z Kamilem</h1>
       <p>Witaj! To jest Twoja pierwsza własna strona 🌟</p>
+
+      <img 
+        src="/celebryta.jpg" 
+        alt="celebryta" 
+        style={{ width: "200px", marginTop: "1rem" }} 
+      />
+      
+      <p>🎁 Życzymy Ci wszystkiego najlepszego!</p>
     </div>
   );
 }
